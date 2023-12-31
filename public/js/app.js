a7a785281ef90b8857b5cfe3364e77ad7bfd2594
ie-orphane <ilyasselyatime0@gmail.com>
2023-12-31 11:47:37 +0100

    small fix countdown restart

--- public/js/app.js
+++ public/js/app.js
@@ -266,7 +266,7 @@ class CountDown {
                 <svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 310 500" fill="#e1e1e1">
                     <path d="M73 39c-14.8-9.1-33.4-9.4-48.5-.9S0 62.6 0 80V432c0 17.4 9.4 33.4 24.5 41.9s33.7 8.1 48.5-.9L361 297c14.3-8.7 23-24.2 23-41s-8.7-32.2-23-41L73 39z" />
                 </svg>`
-            CountDown.restart.firstElementChild.setAttribute('fill', '#ddd')
+            CountDown.restart.firstElementChild.setAttribute('fill', '#7b7b7b')
 
             let currentTime = CountDown.timestamp_to_time(CountDown.time)
             document.querySelector('.countdown-time').innerHTML = CountDown.time_to_string(currentTime)
