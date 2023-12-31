9ae6d4d7877db7248df92f5c7710c45a6109d8de
ie-orphane <ilyasselyatime0@gmail.com>
2023-12-31 11:37:36 +0100

    update

--- public/js/app.js
+++ public/js/app.js
@@ -246,9 +246,9 @@ class CountDown {
                     <path d="M73 39c-14.8-9.1-33.4-9.4-48.5-.9S0 62.6 0 80V432c0 17.4 9.4 33.4 24.5 41.9s33.7 8.1 48.5-.9L361 297c14.3-8.7 23-24.2 23-41s-8.7-32.2-23-41L73 39z" />
                 </svg>`
         } else if (CountDown.timestamp != 0) {
-            if (CountDown.timestamp == CountDown.time) {
-                new Audio("./public/sounds/tick.mp3").play()
-            }
+            // if (CountDown.timestamp == CountDown.time) {
+            //     new Audio("./public/sounds/tick.mp3").play()
+            // }
             CountDown.timer = true
             CountDown.restart.firstElementChild.setAttribute('fill', '#7b7b7b')
             CountDown.play.innerHTML = `
